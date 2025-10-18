ae530852eafe1eccc63b586748585679f7e992ea
Sachin Kumar <46355210+Sachin121Kum@users.noreply.github.com>
2025-10-18 13:17:43 +0530

    Add files via upload

---
+++ deliveryboy/deliveryboy/src/main/java/com/deliveryboy/controller/LocationController.java
@@ -0,0 +1,36 @@
+package com.deliveryboy.controller;
+
+import com.deliveryboy.service.KafkaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/location")
+public class LocationController {
+
+
+    @Autowired
+    private KafkaService kafkaService;
+
+    @PostMapping("/update")
+    public ResponseEntity<?> updateLocation(){
+
+        for(int i =1; i<= 10000000; i++){
+
+            this.kafkaService.updateLocation(
+                    "(" + Math.round(Math.random() * 100) + "," + Math.round(Math.random() * 100) + ")"
+            );
+        System.out.println("_______________________________");
+    }
+
+
+        return  new ResponseEntity<>(Map.of("Message","Location Updated"), HttpStatus.OK);
+    }
+
+}
